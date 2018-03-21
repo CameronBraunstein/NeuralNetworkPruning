9abcf7ff22d1236a189711d4303de0c88f635ef5
CameronBraunsteinb <cbraunstein@brandeis.edu>
2018-03-21 22:12:00 +0530

    broke 94 percent

--- Layer.py
+++ Layer.py
@@ -10,7 +10,7 @@ def sigmoid_prime(x):
     return sigmoid(x)*(1-sigmoid(x))
 
 class Layer:
-    def __init__(self,num_inputs=0,num_outputs=0,W=None,b=None):
+    def __init__(self,num_inputs=0,num_outputs=0,W=None,b=None,l_obs_threshold=0):
         if W is not None and b is not None:
             self.W = W
             self.b = np.reshape(b,(-1,len(b)))
@@ -20,6 +20,7 @@ class Layer:
 
         self.gradient_W = np.zeros(self.W.shape)
         self.gradient_b = np.zeros(self.b.shape)
+        self.l_obs_threshold=0
 
     def forward(self, X):
         self.X = X
@@ -43,41 +44,50 @@ class Layer:
 
         return new_delta
 
-    def build_gradient(self,delta):
-        print 'building_gradient'
-        t = time()
-        partial_derivative_matrix = self.output*(1-self.output)*delta
-        print 'pdm built', time() -t
+    # def build_gradient(self,delta):
+    #     print 'building_gradient'
+    #     t = time()
+    #     partial_derivative_matrix = self.output*(1-self.output)*delta
+    #     print 'pdm built', time() -t
+    #
+    #     t = time()
+    #     self.gradient_W += (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T,self.X).T
+    #     print 'g_W built', time() -t
+    #
+    #     t = time()
+    #     self.gradient_b += (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T, np.ones((partial_derivative_matrix.shape[0],1))).T
+    #     print 'g_b built', time() -t
+    #
+    #
+    #     t = time()
+    #     new_delta=new_delta_scalar*np.matmul(partial_derivative_matrix,self.W.T)
+    #     print 'nd built', time() -t
+    #
+    #     return new_delta
 
-        t = time()
-        self.gradient_W += (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T,self.X).T
-        print 'g_W built', time() -t
 
-        t = time()
-        self.gradient_b += (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T, np.ones((partial_derivative_matrix.shape[0],1))).T
-        print 'g_b built', time() -t
+    # def update(self,learning_rate, l2=1e-5):
+    #     #Add regularizer
+    #     self.gradient_W *= learning_rate
+    #     self.gradient_b *= learning_rate
+    #
+    #     self.gradient_W += l2*self.W
+    #
+    #     #UPDATE WEIGHTS
+    #     self.W -= self.gradient_W
+    #     self.b -= self.gradient_b
+    #
+    #     self.gradient_W.fill(0)
+    #     self.gradient_b.fill(0)
 
+    def calculate_hessian(self):
+        print 'implement'
 
-        t = time()
-        new_delta=new_delta_scalar*np.matmul(partial_derivative_matrix,self.W.T)
-        print 'nd built', time() -t
+    def l_obs(self,):
+        self.delta_W = 0
+        value = 0
 
-        return new_delta
-
-
-    def update(self,learning_rate, l2=1e-5):
-        #Add regularizer
-        self.gradient_W *= learning_rate
-        self.gradient_b *= learning_rate
-
-        self.gradient_W += l2*self.W
-
-        #UPDATE WEIGHTS
-        self.W -= self.gradient_W
-        self.b -= self.gradient_b
-
-        self.gradient_W.fill(0)
-        self.gradient_b.fill(0)
+        return value
 
 
 
