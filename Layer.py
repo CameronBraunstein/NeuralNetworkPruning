7ffd61d9561b9c33fe5c76cd12d7ba7a59c842d3
abhishek-nair-13 <abhisheknair@iisc.ac.in>
2018-03-25 17:22:13 +0530

    Changes to n2ps

--- Layer.py
+++ Layer.py
@@ -11,7 +11,7 @@ def sigmoid_prime(x):
     return sigmoid(x)*(1-sigmoid(x))
 
 class Layer:
-    def __init__(self,num_inputs=0,num_outputs=0,W=None,b=None,l_obs_threshold=0):
+    def __init__(self,num_inputs=0,num_outputs=0,W=None,b=None,l_obs_threshold=0,retain_mask=False):
         if W is not None and b is not None:
             self.W = W
             self.b = np.reshape(b,(-1,len(b)))
@@ -21,9 +21,15 @@ class Layer:
 
         self.gradient_W = np.zeros(self.W.shape)
         self.gradient_b = np.zeros(self.b.shape)
-        self.l_obs_threshold=l_obs_threshold
-        self.delta_W=np.zeros(self.W.shape)
-        self.unpruned_W = np.ones(self.W.shape)
+        self.threshold=l_obs_threshold**2
+        self.losses = np.zeros(self.W.shape[0]*self.W.shape[1])
+        self.delta_W=np.zeros((self.W.shape[0],1))
+
+        if retain_mask:
+            self.unpruned_W = np.copy(self.W)
+            self.unpruned_W[self.unpruned_W!=0] = 1
+        else:
+            self.unpruned_W = np.ones(self.W.shape)
 
     def forward(self, X):
         self.X = X
@@ -31,12 +37,14 @@ class Layer:
         self.output = sigmoid(self.Z)
         return self.output
 
-    def backward(self,delta,learning_rate,l2=1e-5,new_delta_scalar=1):
+    def backward(self,delta,learning_rate,l2=1e-5,new_delta_scalar=22000):
 
         partial_derivative_matrix = self.output*(1-self.output)*delta
 
         gradient_W = (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T,self.X).T + l2*self.W #Include l2
+        #gradient_W = np.matmul(partial_derivative_matrix.T,self.X).T + l2*self.W #Include l2
         gradient_b = (float(1)/self.X.shape[0])*np.matmul(partial_derivative_matrix.T, np.ones((partial_derivative_matrix.shape[0],1))).T
+        #gradient_b = np.matmul(partial_derivative_matrix.T, np.ones((partial_derivative_matrix.shape[0],1))).T
 
         new_delta=new_delta_scalar*np.matmul(partial_derivative_matrix,self.W.T)
 
@@ -49,30 +57,35 @@ class Layer:
 
     def calculate_sub_inverse_hessian(self):
         self.sub_inverse_hessian = gen_inverse(self.X)
+        self.loss_matrix = self.W*self.W / np.diag(self.sub_inverse_hessian).reshape(len(self.sub_inverse_hessian),-1)
+
+        #Ensure nothing goes above the threshold
+        self.loss_matrix[np.where(self.loss_matrix>self.threshold)] = float("inf")
+
+        #Set already pruned weights to inf
+        for i in range(self.unpruned_W.shape[0]):
+            for j in range(self.unpruned_W.shape[1]):
+                if self.unpruned_W[i][j] == 0:
+                    self.loss_matrix[i][j]= float("inf")
+        loss_list = self.loss_matrix.reshape(-1).argsort()
+
+
+        self.loss_indices = iter([ [i//self.loss_matrix.shape[1],i%self.loss_matrix.shape[1]] for i in loss_list])
+
 
     def calculate_loss(self):
-        loss= float("inf")
-        self.i_j = [-1,-1]
-        for i in range(self.sub_inverse_hessian.shape[0]):
-            for j in range(self.W.shape[1]):
-                if self.unpruned_W[i][j]==1:
-                    value=0.5*(self.W[i][j])**2/self.sub_inverse_hessian[i][i]
-                    if value<loss and math.sqrt(value)<self.l_obs_threshold:
-                        loss = value
-                        self.i_j = [i,j]
+        self.i_j = next(self.loss_indices)
+        print (self.i_j)
+        i,j = self.i_j[0],self.i_j[1]
+        self.delta_W = (-float(self.W[i][j]) / self.sub_inverse_hessian[i][i])*self.sub_inverse_hessian[:,i]
+        return self.loss_matrix[i,j]
 
         #Calculate delta
-        if loss == float("inf"):
-            return loss
-
-        self.delta_W.fill(0.0)
-        self.delta_W[:,self.i_j[1]] = (-float(self.W[self.i_j[0]][self.i_j[1]]) /self.sub_inverse_hessian[self.i_j[0]][self.i_j[0]])*self.sub_inverse_hessian[:,self.i_j[0]]
-
-        return loss
 
     def prune(self):
-        print (self.i_j, self.W[self.i_j[0]][self.i_j[1]])
-        self.W += self.unpruned_W*self.delta_W
+        i,j = self.i_j[0],self.i_j[1]
+        self.W[:,j] += self.unpruned_W[:,j]*self.delta_W
+
         #Ensure that i,j is changed to zero (Sometimes a rounding error will effect this)
-        self.W[self.i_j[0]][self.i_j[1]] = 0
-        self.unpruned_W[self.i_j[0]][self.i_j[1]] = 0
+        self.W[i][j] = 0
+        self.unpruned_W[i][j] = 0
--- Network.py
+++ Network.py
@@ -3,40 +3,29 @@ import numpy as np
 import DataLoader as dl
 import FileLoader as fl
 import Layer as l
-import n2ps_algo as n2ps
 
 from time import time
 
-def calculate_error(difference):
-    error = 0
-    for i in range(difference.shape[0]):
-        error += np.dot(difference[i],difference[i])
-    print (error/difference.shape[0])
-
-def calculate_accuracy(outputs, labels):
-    correct = 0
-    for i in range(outputs.shape[0]):
-        if labels[i][np.argmax(outputs[i])] == 1:
-            correct +=1
-            #print labels[i], outputs[i]
-    print (correct/ outputs.shape[0])
-
+# Function to get accuracy and error
 def calculate_accuracy_and_error(outputs,labels):
+    
     difference = outputs-labels
     error = 0
     for i in range(difference.shape[0]):
         error += np.dot(difference[i],difference[i])
-    print ('Error:', error/difference.shape[0])
+    error = error/difference.shape[0]
 
     correct = 0
     for i in range(outputs.shape[0]):
         if labels[i][np.argmax(outputs[i])] == 1:
             correct +=1
-    print ('Accuracy:', float(correct)/ outputs.shape[0])
+    accuracy = float(correct)/ outputs.shape[0]
+    
+    return error, accuracy
 
-
+# Class which has the structure of network for MNIST 
 class Network:
-    def __init__(self,layer_sizes=[784,300,100,10],learning_rate=1e-4,from_file=None,epsilons=[0,0,0]):
+    def __init__(self,layer_sizes=[784,300,100,10],learning_rate=1e-4,from_file=None,retain_mask=True,epsilons=[1,1,1]):
         self.learning_rate = learning_rate
         self.epsilons = epsilons
         self.layers = []
@@ -44,55 +33,54 @@ class Network:
 
         if from_file!=None:
             arrays = fl.return_arrays(from_file)
-            for i in range(int(len(arrays)/2)):
-                self.layers.append(l.Layer(W = arrays[2*i], b=arrays[2*i+1],l_obs_threshold=epsilons[i]))
+            for i in range(len(arrays)//2):
+                self.layers.append(l.Layer(W = arrays[2*i], b=arrays[2*i+1],l_obs_threshold=epsilons[i],retain_mask=retain_mask))
         else:
             for i in range(len(layer_sizes)-1):
                 self.layers.append(l.Layer(num_inputs=layer_sizes[i],num_outputs=layer_sizes[i+1],l_obs_threshold=epsilons[i]))
 
         self.train_images, self.train_labels = self.dl.get_training()
+        self.test_images,self.test_labels = self.dl.get_testing()
 
-    def test(self):
-        test_images,test_labels = self.dl.get_testing()
-        outputs = self.forward(test_images)
-        calculate_accuracy_and_error(outputs,test_labels)
+#Check accuracy over training data
+    def test_training(self):
+        outputs = self.forward(self.train_images)
+        return calculate_accuracy_and_error(outputs,self.train_labels)
 
+#Check accuracy over test data
+    def test_testing(self):
+        
+        outputs = self.forward(self.test_images)
+        return calculate_accuracy_and_error(outputs,self.test_labels)
 
-    def train(self,iterations=300,save_filename=None):
-        t = time()
+# Training the data
+    def train(self,iterations=10,batches=60000,save_filename=None):
+        indices = range(0,self.train_images.shape[0]+1,self.train_images.shape[0]//batches)
+
         for i in range(iterations):
-            outputs = self.forward(self.train_images)
-
-            delta_outputs = outputs-self.train_labels
-
-            if i %1 ==0:
-                print(i)
-                calculate_error(delta_outputs)
-                calculate_accuracy(outputs,self.train_labels)
-
-
-            self.backward(delta_outputs)
-
-        print ('time', time()- t)
-
-        if save_filename is not None:
-            fl.store_arrays(save_filename,self.layers)
-
+            if i % 20 == 0:
+                self.shuffle_samples()
+            for j in range(len(indices)-1):
+                outputs = self.forward(self.train_images[j:j+1])
+                delta_outputs = outputs-self.train_labels[j:j+1]
+                self.backward(delta_outputs)
+            print (self.test_testing())
 
 
     def forward(self,inputs):
         neuron_layer = inputs
+        
         for i in range(len(self.layers)):
             neuron_layer = self.layers[i].forward(neuron_layer)
         return neuron_layer
 
     def backward(self,delta_outputs):
         delta = delta_outputs
+        
         for i in range(1,len(self.layers)):
-            delta = self.layers[-i].backward(delta,self.learning_rate,new_delta_scalar=220000)
+            delta = self.layers[-i].backward(delta,self.learning_rate)
 
-    def l_obs_prune(self, save_filename=None,max_time=50,recalculate_hessian=500):
-
+    def l_obs_prune(self, save_filename=None,max_time=10,recalculate_hessian=200,measure=500):
         self.forward(self.train_images)
         self.test()
         last_pruned_layer=0
@@ -102,13 +90,16 @@ class Network:
         for layer in self.layers:
             layer.calculate_sub_inverse_hessian()
 
-        losses=[float("inf")]*len(self.layers)
-        for i in range(1,len(self.layers)):
-            losses[i] = self.layers[i].calculate_loss()
+        losses = [layer.calculate_loss() for layer in self.layers]
 
         t = time()
         iterations = 0
-        while True and time()-t< max_time:
+
+        weights = self.calculate_weights()
+
+        errors_and_accuracies = -np.ones((weights/measure,2))
+        #while True and time()-t< max_time:
+        while True:
 
             #Calculate losses
             losses[last_pruned_layer] = self.layers[last_pruned_layer].calculate_loss()
@@ -116,14 +107,18 @@ class Network:
             #Find smallest loss
             min_loss = min(losses)
             if min_loss == float("inf"):
-                print ('no more prunable weights')
-                return
-            index = losses.index(min_loss)
-            print ('Layer to Prune:', index)
-            self.layers[index].prune()
-            last_pruned_layer=index
+                print ('no more prunable weights', iterations, time()-t)
 
-            self.test()
+                return
+
+            last_pruned_layer = losses.index(min_loss)
+            self.layers[last_pruned_layer].prune()
+
+            if iterations % measure ==0:
+                results = self.test()
+                errors_and_accuracies[iterations/measure,:] = results
+                print (results, iterations)
+
             iterations += 1
 
             #When the algorithm has run for long enough, recalculate the hessians
@@ -136,9 +131,20 @@ class Network:
                     losses[i] = self.layers[i].calculate_loss()
 
 
+        print (time() - t)
+
+
         if save_filename is not None:
             fl.store_arrays(save_filename,self.layers)
+        print (errors_and_accuracies)
+        np.savetxt('report.txt',errors_and_accuracies)
 
+    def shuffle_samples(self):
+        permutation = np.random.permutation(self.train_images.shape[0])
+        self.train_images = self.train_images[permutation]
+        self.train_labels = self.train_labels[permutation]
+
+# Algorithm to prune input neurons           
     def N2PS_prune_input_neurons(self):
         X = self.train_images
         W = self.layers[0].W
@@ -146,16 +152,21 @@ class Network:
         txip = X.sum(axis=0)
         ftxip = l.sigmoid(abs(txip))
         si = np.zeros(num_inp_neurons)
-        alpha = 0
+        alpha = 0 # Threshold variable
+        
         for i in range(num_inp_neurons):
             for j in range(W.shape[1]):
                 si[i] += abs(ftxip[i] + W[i][j])
-            alpha += si[i]    
-        alpha = alpha/num_inp_neurons
+            alpha += si[i]  
+            
+        alpha = alpha/num_inp_neurons #Normalizing 
+        
         for i in range(X.shape[1]):
             if si[i] <= alpha:
                 for j in range(W.shape[1]):
                     W[i][j] = 0
+                    self.layers[0].unpruned_W[i][j] = 0
+                    
         self.layers[0].W =  W
 
     def N2PS_prune_hidden_neurons(self,ftnet,layer=0):
@@ -171,6 +182,7 @@ class Network:
             tnetjl = temp_tnet.sum(axis=0)
         else:
             X_inp = ftnet
+
             for i in range(X_inp.shape[0]):
                 tnetjl += X_inp[i]* inp_Wt[i]       
       
@@ -179,32 +191,44 @@ class Network:
     
         for s in range(num_hid_neurons):
             ftnetjl = l.sigmoid(abs(tnetjl[s]))
+            
             for m in range(out_Wt.shape[1]):
                 si[s] += abs(ftnetjl + out_Wt[s][m])
             ftnet[s] = ftnetjl
     
         beta = si.sum(axis=0)
         beta = beta/num_hid_neurons    
+        
         for i in range(num_hid_neurons):
             if si[i] <= beta:
                 for k in range(inp_Wt.shape[0]):
                     inp_Wt[k][i] = 0
+                    self.layers[layer].unpruned_W[k][i] = 0
                 for j in range(out_Wt.shape[1]):
                     out_Wt[i][j] = 0
+                    self.layers[layer+1].unpruned_W[i][j] = 0
+                    
+        self.layers[layer].W = inp_Wt 
         self.layers[layer+1].W = out_Wt 
+        
         return ftnet
 
 
+n = Network()
+print("Before Pruning")
+n.train()
+#n = Network(from_file = 'save.txt',learning_rate=1e-4,epsilons=[1e-0,1e-0,1e-0])
+print("Test_training")
+print(n.test_training())
 
-#n = Network()
-
-n = Network(from_file = 'save.txt',learning_rate=1e-4,epsilons=[1e-0,1e-0,1e-0])
-#n.train(save_filename='save.txt')
-#n.l_obs_prune(save_filename='l_obs.txt')
-n.test()
 n.N2PS_prune_input_neurons() #Pruning of input layer
+
 ftnet = 0
+
 for layer in range(len(n.layers)-1):
     ftnet = n.N2PS_prune_hidden_neurons(ftnet,layer=layer) #Pruning of hidden layer
+    
+print("After Pruning")
 n.train()
-n.test()
+print("Test_training")
+print(n.test_training())
